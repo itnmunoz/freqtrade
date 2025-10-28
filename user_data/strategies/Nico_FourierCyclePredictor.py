d7f816c6bd6a40d0cad266cbeabc6f4f84dcea49
Nicolas Muñoz <itnmunoz@gmail.com>
2025-10-28 23:00:23 +0100

    Fourier Strategy implemented 2

--- user_data/strategies/Nico_FourierCyclePredictor.py
+++ user_data/strategies/Nico_FourierCyclePredictor.py
@@ -3,6 +3,7 @@ from pandas import DataFrame
 from scipy.signal import savgol_filter, argrelextrema
 import numpy as np
 
+
 class FourierCycleInflection(IStrategy):
     timeframe = '5m'
     startup_candle_count = 300
@@ -20,7 +21,8 @@ class FourierCycleInflection(IStrategy):
         reconstructed = np.fft.ifft(fft_filtered).real
         t_next = len(signal)
         prediction = sum(
-            np.abs(fft[i]) * np.cos(2 * np.pi * freqs[i] * t_next + np.angle(fft[i]))
+            np.abs(fft[i]) * np.cos(2 * np.pi *
+                                    freqs[i] * t_next + np.angle(fft[i]))
             for i in idx
         )
         return reconstructed, prediction
@@ -40,7 +42,8 @@ class FourierCycleInflection(IStrategy):
             # Detectar mínimos locales en la señal reconstruida
             min_idx = argrelextrema(reconstructed, np.less_equal, order=5)[0]
             df.loc[df.index[-256:], 'cycle_min'] = np.nan
-            df.loc[df.index[-256:][min_idx], 'cycle_min'] = reconstructed[min_idx]
+            df.loc[df.index[-256:][min_idx],
+                   'cycle_min'] = reconstructed[min_idx]
 
             # Calcular pendiente de la señal reconstruida
             slope = np.gradient(reconstructed)
@@ -56,8 +59,19 @@ class FourierCycleInflection(IStrategy):
             (
                 (df['fourier_pred'] > df['close']) &
                 (df['cycle_slope'] > 0) &  # tramo creciente
-                (df['cycle_min'].notnull().shift(1))  # venimos de un mínimo reciente
+                # venimos de un mínimo reciente
+                (df['cycle_min'].notnull().shift(1))
             ),
             'buy'
+        ] = 1
+        return df
+
+    def populate_sell_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
+        df.loc[
+            (
+                (df['fourier_pred'] < df['close']) &
+                (df['cycle_slope'] < 0)  # tramo descendente
+            ),
+            'sell'
         ] = 1
         return df
