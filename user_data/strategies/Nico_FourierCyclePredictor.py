155b09eaf03af43da3321f76d340a3982c6fb4b5
Nicolas Muñoz <itnmunoz@gmail.com>
2025-11-15 23:11:37 +0100

    Causal Strategy 3 Read the doc! Patch 10

--- user_data/strategies/Nico_FourierCyclePredictor.py
+++ user_data/strategies/Nico_FourierCyclePredictor.py
@@ -194,8 +194,8 @@ class FourierCycleInflection(IStrategy):
         )
 
         # BIT OR
-        df['inflection'] = prediction_0 | prediction_1 | regression_1
-        
+        df['inflection'] = prediction_0 | prediction_1
+
         df['enter_long'] = df['inflection'].astype(int)
 
         # Asignar etiqueta alineada con la señal adelantada
